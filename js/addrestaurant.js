87612084b92e03bcec0910221adc3638b925cac5
Carlos Torres <andyandroid412@gmail.com>
2019-06-08 09:39:45 -0500

    first commit to heroku

---
+++ js/addrestaurant.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+         $("#addrestaurantbutton").click( function()
+           {
+             var obj = $('form').serializeJSON();
+             var formData = JSON.stringify(obj);
+
+            alert(formData);
+
+            $.ajax({
+        type: 'POST',
+        url: 'https://loginbackend.herokuapp.com/addfondas',
+        dataType: 'json',
+        data: formData,
+        contentType : 'application/json',
+        success: function(data) {
+            alert(data)
+        }
+    });
+            // hljs.initHighlightingOnLoad(); //function of annotation
+           });
+    });
