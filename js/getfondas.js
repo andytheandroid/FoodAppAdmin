470ae9eccd0afff32402645869ae5a79301c6f02
Carlos Torres <andyandroid412@gmail.com>
2019-06-11 00:11:03 -0500

    new dashboard screen via mdb boostrap framework

---
+++ js/getfondas.js
@@ -0,0 +1,42 @@
+$(document).ready( function () {
+
+
+  $.ajax({
+             type: "POST", //rest Type
+             dataType: 'json',
+             url: "https://loginbackend.herokuapp.com/getfondas",
+             async: false,
+             contentType: "application/json; charset=utf-8",
+             success: function (msg) {
+               var obj = JSON.stringify(msg);
+                drawTable(msg);
+
+             }
+  });
+
+
+});
+
+function drawTable(data) {
+    for (var i = 0; i < data.length; i++) {
+        drawRow(data[i]);
+    }
+}
+
+function drawRow(rowData) {
+    var row = $("<tr />")
+    $("#dataTable").append(row);
+    row.append($("<td>" + rowData.name + "</td>"));
+    row.append($("<td>" + rowData.address + "</td>"));
+    row.append($("<td>" + rowData.description + "</td>"));
+    row.append($("<td>" + rowData.priceLevel + "</td>"));
+    row.append($("<td>" + rowData.coordlatitude + "</td>"));
+    row.append($("<td>" + rowData.coordAltitude + "</td>"));
+    row.append($("<td>" + rowData.neigborhood+ "</td>"));
+
+
+
+
+
+
+}
